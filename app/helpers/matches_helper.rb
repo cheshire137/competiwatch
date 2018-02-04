f1ba5ce6990f2ada3e7c86959a7b3b5965c0a448
Sarah Vessels <cheshire137@gmail.com>
2018-02-04 15:42:11 -0600

    Flesh out match form

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -3,4 +3,9 @@ module MatchesHelper
     valid_maps = @maps.map { |map| [map.name, map.id] }
     [['Choose a map', '']] + valid_maps
   end
+
+  def result_options
+    valid_results = Match::RESULT_MAPPINGS.map { |value, key| [value.to_s.capitalize, key] }
+    [['Choose an outcome', '']] + valid_results
+  end
 end
