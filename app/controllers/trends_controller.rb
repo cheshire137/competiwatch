98f602539f1c5db00221c8537eed6a60d9b5eab5
Sarah Vessels <cheshire137@gmail.com>
2018-02-07 22:55:45 -0600

    Add streaks chart

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -5,7 +5,7 @@ class TrendsController < ApplicationController
   layout false
 
   def per_map_win_loss_chart
-    maps_by_id = Map.order(:name).select([:id, :name, :color]).
+    maps_by_id = Map.order(:name).select([:id, :name]).
       map { |map| [map.id, map] }.to_h
     matches = @oauth_account.matches.in_season(@season).select([:map_id, :result])
 
@@ -24,7 +24,6 @@ class TrendsController < ApplicationController
     end
 
     @map_names = maps_by_id.values.map(&:name)
-    @colors = maps_by_id.values.map(&:color)
     @win_counts = maps_by_id.keys.map { |map_id| wins_by_map_id[map_id] || 0 }
     @loss_counts = maps_by_id.keys.map { |map_id| losses_by_map_id[map_id] || 0 }
     @draw_counts = maps_by_id.keys.map { |map_id| draws_by_map_id[map_id] || 0 }
@@ -35,5 +34,15 @@ class TrendsController < ApplicationController
     @win_count = matches[Match::RESULT_MAPPINGS[:win]]
     @loss_count = matches[Match::RESULT_MAPPINGS[:loss]]
     @draw_count = matches[Match::RESULT_MAPPINGS[:draw]]
+  end
+
+  def streaks_chart
+    matches = @oauth_account.matches.in_season(@season).includes(:prior_match).
+      ordered_by_time.to_a
+    set_streaks(matches)
+
+    @game_numbers = (1..matches.size).to_a
+    @win_streaks = matches.map { |match| match.win_streak || 0 }
+    @loss_streaks = matches.map { |match| match.loss_streak || 0 }
   end
 end
--- app/javascript/packs/charts.js
+++ app/javascript/packs/charts.js
@@ -3,9 +3,49 @@ import SelectorObserver from 'selector-observer'
 import {on} from 'delegated-events'
 import remoteLoadCharts from './remote-load-charts.js'
 
+const winColor = '#29fd2f'
+const transparentWinColor = 'rgba(41,253,47,0.7)'
+const lossColor = '#ca0813'
+const transparentLossColor = 'rgba(202,8,19,0.7)'
+const drawColor = '#fed86f'
+const transparentDrawColor = 'rgba(254,216,111,0.7)'
+
 on('click', '.js-trends-tab', function(event) {
   remoteLoadCharts()
 })
+
+const streaksObserver = new SelectorObserver(document, '.js-streaks-chart', function() {
+  const context = this.getContext('2d')
+  const options = {}
+  const gameNumbers = this.getAttribute('data-game-numbers')
+  const winStreaks = this.getAttribute('data-win-streaks')
+  const lossStreaks = this.getAttribute('data-loss-streaks')
+  const data = {
+    labels: JSON.parse(gameNumbers),
+    datasets: [
+      {
+        fill: 'origin',
+        label: 'Win Streak',
+        backgroundColor: transparentWinColor,
+        borderColor: winColor,
+        borderWidth: 2,
+        data: JSON.parse(winStreaks),
+        pointRadius: 0
+      },
+      {
+        fill: 'origin',
+        label: 'Loss Streak',
+        backgroundColor: transparentLossColor,
+        borderColor: lossColor,
+        borderWidth: 2,
+        data: JSON.parse(lossStreaks),
+        pointRadius: 0
+      }
+    ]
+  }
+  new Chart(context, { type: 'line', data, options })
+})
+streaksObserver.observe()
 
 const winLossObserver = new SelectorObserver(document, '.js-win-loss-chart', function() {
   const context = this.getContext('2d')
@@ -17,7 +57,8 @@ const winLossObserver = new SelectorObserver(document, '.js-win-loss-chart', fun
     labels: ['Wins', 'Losses', 'Draws'],
     datasets: [
       {
-        backgroundColor: ['#ff6384', '#36a2eb', '#ffce56'],
+        backgroundColor: [transparentWinColor, transparentLossColor, transparentDrawColor],
+        borderColor: [winColor, lossColor, drawColor],
         data: [wins, losses, draws]
       }
     ]
@@ -33,7 +74,6 @@ const perMapWinLossObserver = new SelectorObserver(document, '.js-per-map-win-lo
       xAxes: [{ ticks: { autoSkip: false } }]
     }
   }
-  const colors = this.getAttribute('data-colors')
   const labels = this.getAttribute('data-labels')
   const wins = this.getAttribute('data-wins')
   const losses = this.getAttribute('data-losses')
@@ -42,17 +82,23 @@ const perMapWinLossObserver = new SelectorObserver(document, '.js-per-map-win-lo
     labels: JSON.parse(labels),
     datasets: [
       {
-        backgroundColor: '#ff6384',
+        backgroundColor: transparentWinColor,
+        borderColor: winColor,
+        borderWidth: 2,
         label: 'Wins',
         data: JSON.parse(wins)
       },
       {
-        backgroundColor: '#36a2eb',
+        backgroundColor: transparentLossColor,
+        borderColor: lossColor,
+        borderWidth: 2,
         label: 'Losses',
         data: JSON.parse(losses)
       },
       {
-        backgroundColor: '#ffce56',
+        backgroundColor: transparentDrawColor,
+        borderColor: drawColor,
+        borderWidth: 2,
         label: 'Draws',
         data: JSON.parse(draws)
       }
