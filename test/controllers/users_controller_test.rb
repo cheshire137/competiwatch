4a39d358fa6d9b82c1095c36ccf62e227a2e44eb
Sarah Vessels <cheshire137@gmail.com>
2018-02-08 20:43:53 -0600

    Add settings test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'can view your settings' do
+    oauth_account = create(:oauth_account)
+
+    sign_in_as(oauth_account)
+    get '/settings'
+
+    assert_response :ok
+  end
 end
