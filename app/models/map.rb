1a023389fb08f7d2add524403eacd828e3cdc23c
Sarah Vessels <cheshire137@gmail.com>
2018-02-10 17:58:14 -0600

    Add map type validation

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,8 +1,10 @@
 class Map < ApplicationRecord
   extend FriendlyId
 
+  VALID_MAP_TYPES = %w[assault escort hybrid control].freeze
+
   validates :name, presence: true, uniqueness: true
-  validates :map_type, presence: true
+  validates :map_type, presence: true, inclusion: { in: VALID_MAP_TYPES }
 
   has_many :matches
 
