96948bf3fe26c4d3e93afddfcd366cd0f0253ca3
Sarah Vessels <cheshire137@gmail.com>
2018-02-10 17:55:49 -0600

    Add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'requires battletag' do
+    user = User.new
+
+    refute_predicate user, :valid?
+    assert_includes user.errors.messages[:battletag], "can't be blank"
+  end
+
+  test 'requires unique battletag' do
+    user1 = create(:user)
+    user2 = User.new(battletag: user1.battletag)
+
+    refute_predicate user2, :valid?
+    assert_includes user2.errors.messages[:battletag], 'has already been taken'
+  end
 end
