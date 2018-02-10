e285c4a0a420914afa414f5f7f761399e3ae29b3
Sarah Vessels <cheshire137@gmail.com>
2018-02-10 17:59:44 -0600

    Add map model tests

--- test/models/map_test.rb
+++ test/models/map_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class MapTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'requires name' do
+    map = Map.new
+
+    refute_predicate map, :valid?
+    assert_includes map.errors.messages[:name], "can't be blank"
+  end
+
+  test 'requires unique name' do
+    map1 = create(:map)
+    map2 = Map.new(name: map1.name)
+
+    refute_predicate map2, :valid?
+    assert_includes map2.errors.messages[:name], 'has already been taken'
+  end
+
+  test 'requires map type' do
+    map = Map.new
+
+    refute_predicate map, :valid?
+    assert_includes map.errors.messages[:map_type], "can't be blank"
+  end
+
+  test 'requires valid map type' do
+    map = Map.new(map_type: 'something')
+
+    refute_predicate map, :valid?
+    assert_includes map.errors.messages[:map_type], 'is not included in the list'
+  end
+
+  test 'sets slug based on name' do
+    map = Map.create(name: 'Some Awesome Name', map_type: 'assault')
+
+    assert_equal 'some-awesome-name', map.slug
+  end
 end
