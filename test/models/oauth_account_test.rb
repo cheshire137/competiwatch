a20f53f2bb83f6419d8adc11962369e97fc6b2ef
Sarah Vessels <cheshire137@gmail.com>
2018-02-10 18:20:12 -0600

    Add OauthAccount model tests

--- test/models/oauth_account_test.rb
+++ test/models/oauth_account_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class OauthAccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'requires battletag' do
+    oauth_account = OauthAccount.new
+
+    refute_predicate oauth_account, :valid?
+    assert_includes oauth_account.errors.messages[:battletag], "can't be blank"
+  end
+
+  test 'requires provider' do
+    oauth_account = OauthAccount.new
+
+    refute_predicate oauth_account, :valid?
+    assert_includes oauth_account.errors.messages[:provider], "can't be blank"
+  end
+
+  test 'requires unique uid + provider' do
+    oauth_account1 = create(:oauth_account)
+    oauth_account2 = OauthAccount.new(uid: oauth_account1.uid,
+                                      provider: oauth_account1.provider)
+
+    refute_predicate oauth_account2, :valid?
+    assert_includes oauth_account2.errors.messages[:uid], 'has already been taken'
+  end
 end
