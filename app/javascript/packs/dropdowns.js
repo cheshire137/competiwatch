cac8855272e0475a4d7e684a5e4ae12f79026e1f
Sarah Vessels <cheshire137@gmail.com>
2018-02-10 20:06:37 -0600

    Close dropdown when clicking outside

--- app/javascript/packs/dropdowns.js
+++ app/javascript/packs/dropdowns.js
@@ -5,3 +5,19 @@ on('click', '.js-menu-target', function(event) {
   const container = button.closest('.js-menu-container')
   container.classList.toggle('active')
 })
+
+document.addEventListener('click', function(event) {
+  const activeMenus = Array.from(document.querySelectorAll('.js-menu-container.active'))
+  if (activeMenus.length < 1) {
+    return
+  }
+
+  const clickedEl = event.target
+  const menuContainer = clickedEl.closest('.js-menu-container')
+
+  for (const activeMenu of activeMenus) {
+    if (activeMenu.closest('.js-menu-container') !== menuContainer) {
+      activeMenu.classList.remove('active')
+    }
+  }
+})
