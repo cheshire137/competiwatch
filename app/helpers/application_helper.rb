958bbf934de6eff7b4fe5cdb1eb9b4694583bfbd
Sarah Vessels <cheshire137@gmail.com>
2018-02-11 15:45:40 -0600

    Handle when no battletag, season in URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def url_with(options = {})
     url_for(params.permit(:battletag, :season).merge(options))
   end
+
+  def home_page_path
+    if signed_in?
+      battletag = params[:battletag] || current_user.to_param
+      season = params[:season] || Match::LATEST_SEASON
+      url_with(controller: 'matches', action: 'index', battletag: battletag, season: season)
+    else
+      root_path
+    end
+  end
 end
