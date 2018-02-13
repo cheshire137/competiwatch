624db9e32fcc419ec1a9a4094992058445de2bd6
Sarah Vessels <cheshire137@gmail.com>
2018-02-13 17:48:52 -0600

    Add database_cleaner gem to fix flaky test

--- Gemfile
+++ Gemfile
@@ -48,6 +48,10 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
+group :test do
+  gem 'database_cleaner', '~> 1.6.2'
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,18 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 OmniAuth.config.test_mode = true
+DatabaseCleaner.strategy = :transaction
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods
+
+  setup do
+    DatabaseCleaner.start
+  end
+
+  teardown do
+    DatabaseCleaner.clean
+  end
 
   def mock_bnet_omniauth(uid:, battletag:)
     OmniAuth.config.mock_auth[:bnet] = OmniAuth::AuthHash.new(
