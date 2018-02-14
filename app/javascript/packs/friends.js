7f6f1a4789debe2adddff1abcdd4fdbe910feb27
Sarah Vessels <cheshire137@gmail.com>
2018-02-13 19:49:37 -0600

    List existing friends with checkboxes

--- app/javascript/packs/friends.js
+++ app/javascript/packs/friends.js
@@ -3,7 +3,7 @@ import SelectorObserver from 'selector-observer'
 
 const observer = new SelectorObserver(document, '#friends-list', function() {
   const options = {
-    placeholder: 'List players you grouped with',
+    placeholder: 'List other players in your group',
     hiddenInputName: 'friend_names[]',
     preserveCase: true,
     saveOnBlur: true
