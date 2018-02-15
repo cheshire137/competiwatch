d695df13959c8f0539d72a9b980557fe2fb1c2bb
Sarah Vessels <cheshire137@gmail.com>
2018-02-15 10:15:21 -0600

    Test login redirect

--- test/controllers/login_controller_test.rb
+++ test/controllers/login_controller_test.rb
@@ -6,4 +6,13 @@ class LoginControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :ok
   end
+
+  test 'redirects to match history for latest season when signed in' do
+    oauth_account = create(:oauth_account)
+
+    sign_in_as(oauth_account)
+    get '/'
+
+    assert_redirected_to matches_path(Match::LATEST_SEASON, oauth_account)
+  end
 end
