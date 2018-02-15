e90959e1c09c4478f657ee49a415445bfe48ab7a
Sarah Vessels <cheshire137@gmail.com>
2018-02-14 22:48:47 -0600

    Show win/loss/draw across seasons

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -6,5 +6,11 @@ class SeasonsController < ApplicationController
     @active_seasons = @oauth_account.active_seasons
     @total_matches = @oauth_account.matches.count
     @heroes = @oauth_account.heroes.group(:id).order_by_name
+
+    matches = @oauth_account.matches.select(:result)
+    @total_wins = matches.select(&:win?).size
+    @total_losses = matches.select(&:loss?).size
+    @total_draws = matches.select(&:draw?).size
+    @win_percent = (@total_wins / @total_matches.to_f * 100).round(1)
   end
 end
