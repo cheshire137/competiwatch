b0fada96510dbb8bd5682a6747eb3727b9025841
Sarah Vessels <cheshire137@gmail.com>
2018-02-14 23:21:45 -0600

    Show most winning and most losing

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -15,12 +15,29 @@ class SeasonsController < ApplicationController
     @win_percent = (@total_wins / @total_matches.to_f * 100).round(1)
 
     @friends = matches.flat_map(&:friends).uniq.map(&:name).sort_by { |name| name.downcase }
-    match_counts_by_friend = matches.flat_map(&:friends).inject({}) do |hash, friend|
+    @match_counts_by_friend = matches.flat_map(&:friends).inject({}) do |hash, friend|
       hash[friend.name] ||= 0
       hash[friend.name] += 1
       hash
     end
-    max = match_counts_by_friend.values.max
-    @most_frequent_friends = match_counts_by_friend.select { |name, count| count == max }.keys
+    max = @match_counts_by_friend.values.max
+    @most_frequent_friends = @match_counts_by_friend.select { |name, count| count == max }.keys
+
+    win_counts_by_friend = matches.select(&:win?).flat_map(&:friends).inject({}) do |hash, friend|
+      hash[friend.name] ||= 0
+      hash[friend.name] += 1
+      hash
+    end
+    friends_with_more_than_one_match = @match_counts_by_friend.select { |name, count| count > 1 }.keys
+    @win_rates_by_friend = friends_with_more_than_one_match.inject({}) do |hash, name|
+      friend_wins = win_counts_by_friend[name] || 0
+      hash[name] = (friend_wins / @match_counts_by_friend[name].to_f * 100).round
+      hash
+    end
+    max = @win_rates_by_friend.values.max
+    @most_winning_friends = @win_rates_by_friend.select { |name, pct| pct == max }.keys
+
+    min = @win_rates_by_friend.values.min
+    @most_losing_friends = @win_rates_by_friend.select { |name, pct| pct == min }.keys - @most_winning_friends
   end
 end
