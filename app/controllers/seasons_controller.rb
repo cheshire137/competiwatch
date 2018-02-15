191a04bddc851814d531bb90781d5ac8907f63c9
Sarah Vessels <cheshire137@gmail.com>
2018-02-14 22:59:23 -0600

    Show all-season stats about group members

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -7,10 +7,20 @@ class SeasonsController < ApplicationController
     @total_matches = @oauth_account.matches.count
     @heroes = @oauth_account.heroes.group(:id).order_by_name
 
-    matches = @oauth_account.matches.select(:result)
+    matches = @oauth_account.matches.includes(:friends)
+
     @total_wins = matches.select(&:win?).size
     @total_losses = matches.select(&:loss?).size
     @total_draws = matches.select(&:draw?).size
     @win_percent = (@total_wins / @total_matches.to_f * 100).round(1)
+
+    @friends = matches.flat_map(&:friends).uniq.map(&:name).sort_by { |name| name.downcase }
+    match_counts_by_friend = matches.flat_map(&:friends).inject({}) do |hash, friend|
+      hash[friend.name] ||= 0
+      hash[friend.name] += 1
+      hash
+    end
+    max = match_counts_by_friend.values.max
+    @most_frequent_friends = match_counts_by_friend.select { |name, count| count == max }.keys
   end
 end
