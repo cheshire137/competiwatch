41bafc03886f7497cbbfdb99e34b18e1e461722d
Sarah Vessels <cheshire137@gmail.com>
2018-02-16 16:41:06 -0600

    Test exporting multiple heroes per match

--- test/models/match_exporter_test.rb
+++ test/models/match_exporter_test.rb
@@ -8,7 +8,9 @@ class MatchExporterTest < ActiveSupport::TestCase
     @oauth_account = create(:oauth_account, user: user)
     @friend1 = create(:friend, user: user, name: 'Siege')
     @friend2 = create(:friend, user: user, name: 'Rob')
-    @hero1 = create(:hero)
+    @hero1 = create(:hero, name: 'D.Va')
+    @hero2 = create(:hero, name: 'Soldier: 76')
+    @hero3 = create(:hero, name: 'Zarya')
   end
 
   test 'generates CSV of season matches' do
@@ -17,10 +19,12 @@ class MatchExporterTest < ActiveSupport::TestCase
                     map: nil, prior_match: nil)
     match2 = create(:match, season: season, oauth_account: @oauth_account, rank: 1254,
                     map: @map1, ally_thrower: true, prior_match: match1, time_of_day: :evening)
+    match2.heroes << @hero3
     match3 = create(:match, season: season, oauth_account: @oauth_account, rank: 1273,
                     map: @map2, enemy_leaver: true, prior_match: match2, time_of_day: :morning,
                     day_of_week: :weekday)
     match3.heroes << @hero1
+    match3.heroes << @hero2
     match4 = create(:match, season: season, oauth_account: @oauth_account, rank: 1295,
                     map: @map1, prior_match: match3, comment: 'this is so cool')
     create(:match_friend, match: match4, friend: @friend1)
@@ -35,8 +39,8 @@ class MatchExporterTest < ActiveSupport::TestCase
     assert_equal 'Rank,Map,Comment,Day,Time,Heroes,Ally Leaver,Ally Thrower,Enemy Leaver' +
                  ',Enemy Thrower,Group', lines[0]
     assert_equal %q(1234,,,,,"",,,,,""), lines[1]
-    assert_equal %Q(1254,#{@map1.name},,,evening,"",,Y,,,""), lines[2]
-    assert_equal %Q(1273,#{@map2.name},,weekday,morning,#{@hero1.name},,,Y,,""), lines[3]
+    assert_equal %Q(1254,#{@map1.name},,,evening,#{@hero3.name},,Y,,,""), lines[2]
+    assert_equal %Q(1273,#{@map2.name},,weekday,morning,"#{@hero1.name}, #{@hero2.name}",,,Y,,""), lines[3]
     assert_equal %Q(1295,#{@map1.name},this is so cool,,,"",,,,,"Rob, Siege"), lines[4]
   end
 end
