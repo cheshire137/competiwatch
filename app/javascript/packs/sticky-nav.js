8fe90d30cd231d843c14ae0f7ee5063d454d79cb
Sarah Vessels <cheshire137@gmail.com>
2018-02-15 22:07:28 -0600

    Fix jumping with sticky header on match form tab

--- app/javascript/packs/sticky-nav.js
+++ app/javascript/packs/sticky-nav.js
@@ -2,13 +2,16 @@ import SelectorObserver from 'selector-observer'
 
 function setupStickyNav() {
   const nav = document.querySelector('.js-sticky-nav')
+  const placeholder = document.querySelector('.js-sticky-nav-placeholder')
   const offset = parseInt(nav.getAttribute('data-original-offset'), 10)
-  nav.classList.toggle('sticky-nav', window.pageYOffset >= offset)
+  const isSticky = window.pageYOffset >= offset
+  nav.classList.toggle('sticky-nav', isSticky)
+  placeholder.classList.toggle('d-none', !isSticky)
 }
 
 const observer = new SelectorObserver(document, '.js-sticky-nav', function() {
   const rect = this.getBoundingClientRect()
-  this.setAttribute('data-original-offset', rect.x - 8)
+  this.setAttribute('data-original-offset', rect.top)
   document.addEventListener('scroll', setupStickyNav, { passive: true })
   setupStickyNav()
 })
