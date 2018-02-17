4418e2e4b139c89420dc008ef230778a99e92578
Sarah Vessels <cheshire137@gmail.com>
2018-02-17 11:57:47 -0600

    Update home page URL to use default account

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,8 @@ module ApplicationHelper
 
   def home_page_path
     if signed_in?
-      battletag = params[:battletag] || current_user.to_param
+      battletag = params[:battletag] || current_user.default_oauth_account.try(:to_param) ||
+        current_user.to_param
       season = params[:season] || Match::LATEST_SEASON
       url_with(controller: 'matches', action: 'index', battletag: battletag, season: season)
     else
