3e0263b17bc85f9211267d133ea2dfc8724d1c7a
Sarah Vessels <cheshire137@gmail.com>
2018-02-17 20:27:07 -0600

    Rearrange settings page, add helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,10 @@ module UsersHelper
       [label, zone.name]
     end
   end
+
+  def settings_box(icon_class:, title:, url:, description:)
+    render partial: 'users/settings_box',
+           locals: { icon_class: icon_class, title: title,
+                     url: url, description: description }
+  end
 end
