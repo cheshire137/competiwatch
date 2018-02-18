7f783be85c11e29f83e016e1fb56a95c377c9321
Sarah Vessels <cheshire137@gmail.com>
2018-02-17 20:17:45 -0600

    Add tests for season shares index

--- test/controllers/season_shares_controller_test.rb
+++ test/controllers/season_shares_controller_test.rb
@@ -1,6 +1,35 @@
 require 'test_helper'
 
 class SeasonSharesControllerTest < ActionDispatch::IntegrationTest
+  test 'can view your own shares' do
+    user = create(:user)
+    oauth_account1 = create(:oauth_account, user: user)
+    oauth_account2 = create(:oauth_account, user: user)
+    create(:season_share, oauth_account: oauth_account1, season: 3)
+    create(:season_share, oauth_account: oauth_account1, season: 5)
+    create(:season_share, oauth_account: oauth_account2, season: 7)
+
+    sign_in_as(oauth_account1)
+    get '/shared-seasons'
+
+    assert_response :ok
+    assert_select 'li', text: /Season 1\s+Only visible to you/
+    assert_select 'li', text: /Season 2\s+Only visible to you/
+    assert_select 'li', text: /Season 3\s+Publicly visible/
+    assert_select 'li', text: /Season 4\s+Only visible to you/
+    assert_select 'li', text: /Season 5\s+Publicly visible/
+    assert_select 'li', text: /Season 6\s+Only visible to you/
+    assert_select 'li', text: /Season 7\s+Publicly visible/
+    assert_select 'li', text: /Season 8\s+Only visible to you/
+  end
+
+  test 'requires login for viewing season shares' do
+    get '/shared-seasons'
+
+    assert_response :redirect
+    assert_redirected_to 'http://www.example.com/'
+  end
+
   test 'create redirects for anonymous user' do
     oauth_account = create(:oauth_account)
 
