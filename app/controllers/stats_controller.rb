617bcfbe830695da77d1332a026452b0e517e54d
Sarah Vessels <cheshire137@gmail.com>
2018-02-18 17:46:14 -0600

    DRY up stats controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -8,52 +8,17 @@ class StatsController < ApplicationController
     @active_seasons = @oauth_account.active_seasons
     matches = @oauth_account.matches.includes([:friends, :heroes]).with_result.ordered_by_time
     @total_matches = matches.count
-
-    @match_counts_by_hero = matches.inject({}) do |hash, match|
-      match.heroes.each do |hero|
-        hash[hero] ||= 0
-        hash[hero] += 1
-      end
-      hash
-    end
-    @match_counts_by_hero = @match_counts_by_hero.sort_by { |_hero, count| -count }.to_h
+    @match_counts_by_hero = get_match_counts_by_hero(matches)
     @max_hero_match_count = @match_counts_by_hero.values.first
-
-    all_ranks = matches.select { |match| match.season > 1 }.map(&:rank).sort
-    @lowest_sr = all_ranks.min
-    @highest_sr = all_ranks.max
-
-    @total_wins = matches.select(&:win?).size
-    @total_losses = matches.select(&:loss?).size
-    @total_draws = matches.select(&:draw?).size
-
-    friends = matches.flat_map(&:friends)
-    @friends = friends.uniq.map(&:name).sort_by(&:downcase)
-    @match_counts_by_friend = friends.inject({}) do |hash, friend|
-      hash[friend.name] ||= 0
-      hash[friend.name] += 1
-      hash
-    end
+    @lowest_sr, @highest_sr = get_lowest_and_highest_rank(matches)
+    @total_wins, @total_losses, @total_draws = get_total_wins_losses_draws(matches)
+    @friends, @match_counts_by_friend = get_friends_and_match_counts(matches)
     @most_frequent_match_count = @match_counts_by_friend.values.max
     @most_frequent_friends = @match_counts_by_friend.
       select { |name, count| count == @most_frequent_match_count }.keys
-
-    win_counts_by_friend = matches.select(&:win?).flat_map(&:friends).inject({}) do |hash, friend|
-      hash[friend.name] ||= 0
-      hash[friend.name] += 1
-      hash
-    end
-    friends_with_more_than_one_match = @match_counts_by_friend.select { |name, count| count > 1 }.keys
-    @win_rates_by_friend = friends_with_more_than_one_match.inject({}) do |hash, name|
-      friend_wins = win_counts_by_friend[name] || 0
-      hash[name] = (friend_wins / @match_counts_by_friend[name].to_f * 100).round
-      hash
-    end
-    max = @win_rates_by_friend.values.max
-    @most_winning_friends = @win_rates_by_friend.select { |name, pct| pct == max }.keys
-
-    min = @win_rates_by_friend.values.min
-    @most_losing_friends = @win_rates_by_friend.select { |name, pct| pct == min }.keys - @most_winning_friends
+    @win_rates_by_friend = get_win_rates_by_friend(matches, @match_counts_by_friend)
+    @most_winning_friends = get_most_winning_friends(@win_rates_by_friend)
+    @most_losing_friends = get_most_losing_friends(@win_rates_by_friend, @most_winning_friends)
   end
 
   def all_accounts
@@ -61,51 +26,76 @@ class StatsController < ApplicationController
     @total_matches = matches.count
     @total_accounts = matches.map(&:oauth_account_id).uniq.size
     @account_battletags = matches.map(&:oauth_account).uniq.map(&:battletag).sort_by(&:downcase)
+    @match_counts_by_hero = get_match_counts_by_hero(matches)
+    @max_hero_match_count = @match_counts_by_hero.values.first
+    @lowest_sr, @highest_sr = get_lowest_and_highest_rank(matches)
+    @total_wins, @total_losses, @total_draws = get_total_wins_losses_draws(matches)
+    @friends, @match_counts_by_friend = get_friends_and_match_counts(matches)
+    @most_frequent_match_count = @match_counts_by_friend.values.max
+    @most_frequent_friends = @match_counts_by_friend.
+      select { |name, count| count == @most_frequent_match_count }.keys
+    @win_rates_by_friend = get_win_rates_by_friend(matches, @match_counts_by_friend)
+    @most_winning_friends = get_most_winning_friends(@win_rates_by_friend)
+    @most_losing_friends = get_most_losing_friends(@win_rates_by_friend, @most_winning_friends)
+  end
 
-    @match_counts_by_hero = matches.inject({}) do |hash, match|
+  private
+
+  def get_most_losing_friends(win_rates_by_friend, most_winning_friends)
+    min = win_rates_by_friend.values.min
+    win_rates_by_friend.select { |name, pct| pct == min }.keys - most_winning_friends
+  end
+
+  def get_most_winning_friends(win_rates_by_friend)
+    max = win_rates_by_friend.values.max
+    win_rates_by_friend.select { |name, pct| pct == max }.keys
+  end
+
+  def get_lowest_and_highest_rank(matches)
+    all_ranks = matches.select { |match| match.season > 1 }.map(&:rank).sort
+    [all_ranks.min, all_ranks.max]
+  end
+
+  def get_win_rates_by_friend(matches, match_counts_by_friend)
+    win_counts_by_friend = matches.select(&:win?).flat_map(&:friends).inject({}) do |hash, friend|
+      hash[friend.name] ||= 0
+      hash[friend.name] += 1
+      hash
+    end
+    friends_with_more_than_one_match = match_counts_by_friend.select { |name, count| count > 1 }.keys
+    friends_with_more_than_one_match.inject({}) do |hash, name|
+      friend_wins = win_counts_by_friend[name] || 0
+      hash[name] = (friend_wins / match_counts_by_friend[name].to_f * 100).round
+      hash
+    end
+  end
+
+  def get_friends_and_match_counts(matches)
+    friends_with_dupes = matches.flat_map(&:friends)
+    friends = friends_with_dupes.uniq.map(&:name).sort_by(&:downcase)
+    match_counts_by_friend = friends_with_dupes.inject({}) do |hash, friend|
+      hash[friend.name] ||= 0
+      hash[friend.name] += 1
+      hash
+    end
+    [friends, match_counts_by_friend]
+  end
+
+  def get_total_wins_losses_draws(matches)
+    wins = matches.select(&:win?).size
+    losses = matches.select(&:loss?).size
+    draws = matches.select(&:draw?).size
+    [wins, losses, draws]
+  end
+
+  def get_match_counts_by_hero(matches)
+    result = matches.inject({}) do |hash, match|
       match.heroes.each do |hero|
         hash[hero] ||= 0
         hash[hero] += 1
       end
       hash
     end
-    @match_counts_by_hero = @match_counts_by_hero.sort_by { |_hero, count| -count }.to_h
-    @max_hero_match_count = @match_counts_by_hero.values.first
-
-    all_ranks = matches.select { |match| match.season > 1 }.map(&:rank).sort
-    @lowest_sr = all_ranks.min
-    @highest_sr = all_ranks.max
-
-    @total_wins = matches.select(&:win?).size
-    @total_losses = matches.select(&:loss?).size
-    @total_draws = matches.select(&:draw?).size
-
-    friends = matches.flat_map(&:friends)
-    @friends = friends.uniq.map(&:name).sort_by(&:downcase)
-    @match_counts_by_friend = friends.inject({}) do |hash, friend|
-      hash[friend.name] ||= 0
-      hash[friend.name] += 1
-      hash
-    end
-    @most_frequent_match_count = @match_counts_by_friend.values.max
-    @most_frequent_friends = @match_counts_by_friend.
-      select { |name, count| count == @most_frequent_match_count }.keys
-
-    win_counts_by_friend = matches.select(&:win?).flat_map(&:friends).inject({}) do |hash, friend|
-      hash[friend.name] ||= 0
-      hash[friend.name] += 1
-      hash
-    end
-    friends_with_more_than_one_match = @match_counts_by_friend.select { |name, count| count > 1 }.keys
-    @win_rates_by_friend = friends_with_more_than_one_match.inject({}) do |hash, name|
-      friend_wins = win_counts_by_friend[name] || 0
-      hash[name] = (friend_wins / @match_counts_by_friend[name].to_f * 100).round
-      hash
-    end
-    max = @win_rates_by_friend.values.max
-    @most_winning_friends = @win_rates_by_friend.select { |name, pct| pct == max }.keys
-
-    min = @win_rates_by_friend.values.min
-    @most_losing_friends = @win_rates_by_friend.select { |name, pct| pct == min }.keys - @most_winning_friends
+    result.sort_by { |_hero, count| -count }.to_h
   end
 end
