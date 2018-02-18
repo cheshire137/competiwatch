216808df717fcc013d3dc5d5b296986400fbd63b
Sarah Vessels <cheshire137@gmail.com>
2018-02-18 17:30:37 -0600

    Make use of record in setup block

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -1,31 +1,32 @@
 require 'test_helper'
 
 class StatsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @oauth_account = create(:oauth_account)
+  end
+
   test 'all seasons page redirects anonymous user' do
-    get '/stats/all-seasons/DPSMain22-1234'
+    get "/stats/all-seasons/#{@oauth_account.to_param}"
 
     assert_response :redirect
     assert_redirected_to 'http://www.example.com/'
   end
 
   test 'all seasons page loads successfully for authenticated user with no matches' do
-    oauth_account = create(:oauth_account)
-
-    sign_in_as(oauth_account)
-    get "/stats/all-seasons/#{oauth_account.to_param}"
+    sign_in_as(@oauth_account)
+    get "/stats/all-seasons/#{@oauth_account.to_param}"
 
     assert_response :ok
     assert_select 'div', text: /Matches logged:\s+0/
   end
 
   test 'all seasons page loads successfully for authenticated user with matches' do
-    oauth_account = create(:oauth_account)
-    create(:match, oauth_account: oauth_account, season: 1, result: :win)
-    create(:match, oauth_account: oauth_account, season: 2, result: :loss)
-    create(:match, oauth_account: oauth_account, season: 3, result: :draw)
+    create(:match, oauth_account: @oauth_account, season: 1, result: :win)
+    create(:match, oauth_account: @oauth_account, season: 2, result: :loss)
+    create(:match, oauth_account: @oauth_account, season: 3, result: :draw)
 
-    sign_in_as(oauth_account)
-    get "/stats/all-seasons/#{oauth_account.to_param}"
+    sign_in_as(@oauth_account)
+    get "/stats/all-seasons/#{@oauth_account.to_param}"
 
     assert_response :ok
     assert_select 'div', text: /Matches logged:\s+3/
@@ -33,10 +34,9 @@ class StatsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "won't let you view all season data for another user's account" do
-    oauth_account1 = create(:oauth_account)
     oauth_account2 = create(:oauth_account)
 
-    sign_in_as(oauth_account1)
+    sign_in_as(@oauth_account)
     get "/stats/all-seasons/#{oauth_account2.to_param}"
 
     assert_response :not_found
