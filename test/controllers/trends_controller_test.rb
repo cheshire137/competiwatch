84e4bd7d8be6cc96a811dde062ccbb1780cfe369
Sarah Vessels <cheshire137@gmail.com>
2018-02-18 16:40:24 -0600

    Add trends controller tests

--- test/controllers/trends_controller_test.rb
+++ test/controllers/trends_controller_test.rb
@@ -1,7 +1,203 @@
 require 'test_helper'
 
 class TrendsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    @oauth_account = create(:oauth_account, user: @user)
+    @match1 = create(:match, oauth_account: @oauth_account, season: 2)
+    @match2 = create(:match, oauth_account: @oauth_account, season: 2,
+                     prior_match: @match1)
+    @friend = create(:friend, user: @user)
+    @match1_friend = create(:match_friend, match: @match1, friend: @friend)
+    @match2_friend = create(:match_friend, match: @match2, friend: @friend)
+  end
+
+  test 'heroes chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/heroes-chart"
+
+    assert_response :not_found
+  end
+
+  test 'heroes chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/heroes-chart"
+
+    assert_response :ok
+  end
+
+  test 'heroes chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/heroes-chart"
+
+    assert_response :ok
+  end
+
+  test 'thrower/leaver chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/thrower-leaver-chart"
+
+    assert_response :not_found
+  end
+
+  test 'thrower/leaver chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/thrower-leaver-chart"
+
+    assert_response :ok
+  end
+
+  test 'thrower/leaver chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/thrower-leaver-chart"
+
+    assert_response :ok
+  end
+
+  test 'day/time chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/day-time-chart"
+
+    assert_response :not_found
+  end
+
+  test 'day/time chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/day-time-chart"
+
+    assert_response :ok
+  end
+
+  test 'day/time chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/day-time-chart"
+
+    assert_response :ok
+  end
+
+  test 'streaks chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/streaks-chart"
+
+    assert_response :not_found
+  end
+
+  test 'streaks chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/streaks-chart"
+
+    assert_response :ok
+  end
+
+  test 'streaks chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/streaks-chart"
+
+    assert_response :ok
+  end
+
+  test 'map chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/map-chart"
+
+    assert_response :not_found
+  end
+
+  test 'map chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/map-chart"
+
+    assert_response :ok
+  end
+
+  test 'map chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/map-chart"
+
+    assert_response :ok
+  end
+
+  test 'win/loss chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/win-loss-chart"
+
+    assert_response :not_found
+  end
+
+  test 'win/loss chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/win-loss-chart"
+
+    assert_response :ok
+  end
+
+  test 'win/loss chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/win-loss-chart"
+
+    assert_response :ok
+  end
+
+  test 'group size chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/group-size-chart"
+
+    assert_response :not_found
+  end
+
+  test 'group size chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/group-size-chart"
+
+    assert_response :ok
+  end
+
+  test 'group size chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/group-size-chart"
+
+    assert_response :ok
+  end
+
+  test 'group member chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/group-member-chart"
+
+    assert_response :not_found
+  end
+
+  test 'group member chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/group-member-chart"
+
+    assert_response :ok
+  end
+
+  test 'group member chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/group-member-chart"
+
+    assert_response :ok
+  end
+
+  test 'role chart 404s for anonymous user when season not shared' do
+    get "/trends/2/#{@oauth_account.to_param}/role-chart"
+
+    assert_response :not_found
+  end
+
+  test 'role chart loads for owner' do
+    sign_in_as(@oauth_account)
+    get "/trends/2/#{@oauth_account.to_param}/role-chart"
+
+    assert_response :ok
+  end
+
+  test 'role chart loads for anonymous user when season is shared' do
+    create(:season_share, oauth_account: @oauth_account, season: 2)
+
+    get "/trends/2/#{@oauth_account.to_param}/role-chart"
+
+    assert_response :ok
+  end
 end
