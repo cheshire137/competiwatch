4a4a254c0113d4ab99be38a29ef8bc819e30ff73
Sarah Vessels <cheshire137@gmail.com>
2018-02-19 12:18:26 -0600

    With new nav, don't need home page URL to change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,15 +22,4 @@ module ApplicationHelper
   def is_page?(controller, action)
     params[:controller] == controller && params[:action] == action
   end
-
-  def home_page_path
-    if signed_in?
-      battletag = params[:battletag] || current_user.default_oauth_account.try(:to_param) ||
-        current_user.to_param
-      season = params[:season] || Match::LATEST_SEASON
-      url_with(controller: 'matches', action: 'index', battletag: battletag, season: season)
-    else
-      root_path
-    end
-  end
 end
