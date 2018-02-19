0d4d3f46459ab6ad94678db2b74992cd22f90099
Sarah Vessels <cheshire137@gmail.com>
2018-02-19 12:43:00 -0600

    Colorize win rate bars for groups

--- app/helpers/trends_helper.rb
+++ app/helpers/trends_helper.rb
@@ -1,2 +1,15 @@
 module TrendsHelper
+  def win_rate_color(win_rate)
+    if win_rate >= 90
+      'rgb(139, 236, 34)'
+    elsif win_rate >= 70
+      '#c4e556'
+    elsif win_rate >= 50
+      '#fdde73'
+    elsif win_rate >= 30
+      '#f0a05b'
+    else
+      '#de4f3b'
+    end
+  end
 end
