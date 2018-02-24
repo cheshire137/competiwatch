1aaae2c80220ac31c5e91bc95a725c68a92983b5
Sarah Vessels <cheshire137@gmail.com>
2018-02-24 12:29:52 -0600

    Adjust color scale for group stats

--- app/helpers/trends_helper.rb
+++ app/helpers/trends_helper.rb
@@ -2,11 +2,11 @@ module TrendsHelper
   def win_rate_color(win_rate)
     if win_rate >= 90
       'rgb(139, 236, 34)'
-    elsif win_rate >= 70
+    elsif win_rate >= 60
       '#c4e556'
     elsif win_rate >= 50
       '#fdde73'
-    elsif win_rate >= 30
+    elsif win_rate >= 40
       '#f0a05b'
     else
       '#de4f3b'
