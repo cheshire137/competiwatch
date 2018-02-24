5856a2da3a435cced230181e27e2acdc4b92be42
Sarah Vessels <cheshire137@gmail.com>
2018-02-24 12:11:55 -0600

    Move chart helpers to trends helper module

--- app/helpers/trends_helper.rb
+++ app/helpers/trends_helper.rb
@@ -12,4 +12,39 @@ module TrendsHelper
       '#de4f3b'
     end
   end
+
+  def show_group_stats?(matches)
+    show_group_member_chart?(matches)
+  end
+
+  def show_map_chart?(matches)
+    matches.any? { |match| match.map.present? }
+  end
+
+  def show_group_size_chart?(matches)
+    show_group_member_chart?(matches)
+  end
+
+  def show_group_member_chart?(matches)
+    return @show_group_member_chart if defined? @show_group_member_chart
+    @show_group_member_chart = matches.any? { |match| match.friends.any? }
+  end
+
+  def show_thrower_leaver_chart?(matches)
+    return @show_thrower_leaver_chart if defined? @show_thrower_leaver_chart
+    @show_thrower_leaver_chart = matches.any? { |match| match.thrower? || match.leaver? }
+  end
+
+  def show_role_chart?(matches)
+    show_heroes_chart?(matches)
+  end
+
+  def show_heroes_chart?(matches)
+    return @show_heroes_chart if defined? @show_heroes_chart
+    @show_heroes_chart = matches.any? { |match| match.heroes.any? }
+  end
+
+  def show_day_time_chart?(matches)
+    matches.any? { |match| match.day_of_week.present? || match.time_of_day.present? }
+  end
 end
