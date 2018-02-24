11c9c9d70267e9def1118eb3478aff58665ec856
Sarah Vessels <cheshire137@gmail.com>
2018-02-24 14:57:19 -0600

    Don't show import option on match page for future season

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -9,6 +9,7 @@ class MatchesController < ApplicationController
   def index
     @can_edit = signed_in? && @oauth_account.user == current_user
     @past_season = Season.past?(@season)
+    @future_season = Season.future?(@season)
     @matches = @oauth_account.matches.in_season(@season).
       includes(:prior_match, :heroes, :map, :friends).ordered_by_time
     set_streaks(@matches)
