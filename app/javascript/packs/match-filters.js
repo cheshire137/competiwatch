303c0e890aefea0c5ccf2d22fe5888929bd73693
Sarah Vessels <cheshire137@gmail.com>
2018-02-24 19:48:24 -0600

    Allow combining filters

--- app/javascript/packs/match-filters.js
+++ app/javascript/packs/match-filters.js
@@ -6,9 +6,17 @@ on('click', '.js-match-filter', function(event) {
   const menuContainer = button.closest('.js-menu-container')
   menuContainer.classList.toggle('active')
 
+  let activeClasses = [classToShow]
+  if (classToShow !== 'js-filterable-match') {
+    const otherClasses = (menuContainer.getAttribute('data-active-filters') || '').
+      split(' ').filter(cls => cls.length > 0)
+    activeClasses = activeClasses.concat(otherClasses)
+  }
+  menuContainer.setAttribute('data-active-filters', activeClasses.join(' '))
+
   const filterButtons = menuContainer.querySelectorAll('.js-match-filter')
   for (const filterButton of filterButtons) {
-    const isSelected = filterButton.getAttribute('data-filter') === classToShow
+    const isSelected = activeClasses.indexOf(filterButton.getAttribute('data-filter')) > -1
     filterButton.classList.toggle('selected', isSelected)
   }
 
@@ -18,7 +26,13 @@ on('click', '.js-match-filter', function(event) {
 
   let visibleCount = 0
   for (const match of matches) {
-    const showMatch = match.classList.contains(classToShow)
+    let showMatch = true
+    for (const filterClass of activeClasses) {
+      showMatch = showMatch && match.classList.contains(filterClass)
+      if (!showMatch) {
+        break
+      }
+    }
     match.classList.toggle('d-none', !showMatch)
     if (showMatch) {
       visibleCount++
