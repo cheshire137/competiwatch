35e9ca28096679bbf28d28d0fa5525d1e285d82d
Sarah Vessels <cheshire137@gmail.com>
2018-03-03 14:16:41 -0600

    Redirect from herokuapp URL to custom domain

--- app/javascript/packs/https-redirect.js
+++ app/javascript/packs/https-redirect.js
@@ -1,4 +1,9 @@
-const host = 'www.competiwatch.com'
-if (host === window.location.host && window.location.protocol !== 'https:') {
+const preferredHost = 'www.competiwatch.com'
+if (preferredHost === window.location.host && window.location.protocol !== 'https:') {
   window.location.protocol = 'https'
+}
+
+const otherHost = 'competiwatch.herokuapp.com'
+if (otherHost === window.location.host) {
+  window.location.href = `https://${preferredHost}`
 }
