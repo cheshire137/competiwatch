0541fb8f965af412973cff5784473dd3e32921b1
Sarah Vessels <cheshire137@gmail.com>
2018-03-03 17:15:33 -0600

    Show friend count on admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,7 @@ class AdminController < ApplicationController
   def index
     all_users = User.order_by_battletag
     @users = all_users.paginate(page: current_page)
+    @friends_by_user_id = Friend.select(:user_id).group_by(&:user_id)
     @oauth_accounts_by_user_id = OauthAccount.order_by_battletag.group_by(&:user_id)
     @seasons = Season.latest_first.select(:number)
     @matches_by_oauth_account_id = Match.select([:season, :oauth_account_id]).
