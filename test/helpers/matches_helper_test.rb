f9ca3172564b55edcf1a13dbfea0b794ec0c6985
Sarah Vessels <cheshire137@gmail.com>
2018-03-03 12:17:13 -0600

    Fix failing green test by getting a gradient from changes to SR, not absolute SR

--- test/helpers/matches_helper_test.rb
+++ test/helpers/matches_helper_test.rb
@@ -3,16 +3,16 @@ require 'test_helper'
 class MatchesHelperTest < ActionView::TestCase
   test 'returns darker green for bigger SR change' do
     match1 = build(:match, rank: 3573, placement: true, result: :loss)
-    match2 = build(:match, rank: 3547, prior_match: match1, result: :loss)
-    match3 = build(:match, rank: 3572, prior_match: match2, result: :win)
-    match4 = build(:match, rank: 3596, prior_match: match3, result: :win)
+    match2 = build(:match, rank: 3547, prior_match: match1, result: :loss) # -26
+    match3 = build(:match, rank: 3572, prior_match: match2, result: :win)  # +25
+    match4 = build(:match, rank: 3596, prior_match: match3, result: :win)  # +24
     matches = [match1, match2, match3, match4]
 
     match3_style = match_rank_change_style(match3, matches)
     assert_equal 'background-color: rgb(102, 189, 125)', match3_style
 
     match4_style = match_rank_change_style(match4, matches)
-    assert_equal 'background-color: rgb(152, 204, 129)', match4_style
+    assert_equal 'background-color: rgb(140, 200, 128)', match4_style
   end
 
   test 'does not calculate rank change from penultimate to last placement match' do
