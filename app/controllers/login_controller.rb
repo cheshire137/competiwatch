6d398162452a4c2f845350ec3e3f4a0d40730387
Sarah Vessels <cheshire137@gmail.com>
2018-03-03 18:43:15 -0600

    Redirect to default account from login page if signed in

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,7 +7,10 @@ class LoginController < ApplicationController
   private
 
   def redirect_if_signed_in
-    season = Season.current_or_latest_number
-    redirect_to(matches_path(season, current_user)) if signed_in?
+    if signed_in?
+      season = Season.current_or_latest_number
+      oauth_account = current_user.default_oauth_account || current_user.oauth_accounts.last
+      redirect_to(matches_path(season, oauth_account))
+    end
   end
 end
