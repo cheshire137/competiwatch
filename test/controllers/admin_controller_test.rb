5c8432e3b120cd7a32aacf80d5bf585ffdd35567
Sarah Vessels <cheshire137@gmail.com>
2018-03-03 18:10:51 -0600

    Test creating, deleting seasons

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -46,6 +46,33 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
     assert_equal seasons(:one).max_rank, season.reload.max_rank
   end
 
+  test 'non-admin users cannot create a season' do
+    user = create(:user)
+    oauth_account = create(:oauth_account, user: user)
+
+    assert_no_difference 'Season.count' do
+      sign_in_as(oauth_account)
+      post '/admin/season', params: {
+        create_season: { max_rank: 1234, number: 3 }
+      }
+    end
+
+    assert_response :not_found
+  end
+
+  test 'non-admin users cannot delete a season' do
+    user = create(:user)
+    oauth_account = create(:oauth_account, user: user)
+    season = seasons(:two)
+
+    assert_no_difference 'Season.count' do
+      sign_in_as(oauth_account)
+      delete '/admin/season', params: { season_id: season.id }
+    end
+
+    assert_response :not_found
+  end
+
   test 'non-admin cannot merge users' do
     primary_user = create(:user)
     secondary_user = create(:user)
@@ -121,6 +148,38 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
     assert_equal 6001, season.reload.max_rank
     assert_equal Date.parse('2018-02-28'), season.started_on
     assert_equal Date.parse('2018-05-15'), season.ended_on
+  end
+
+  test 'admin users can delete seasons' do
+    admin_user = create(:user, admin: true)
+    admin_account = create(:oauth_account, user: admin_user)
+    season = seasons(:two)
+
+    assert_difference 'Season.count', -1 do
+      sign_in_as(admin_account)
+      delete '/admin/season', params: { season_id: season.id }
+    end
+
+    assert_redirected_to admin_path
+    assert_equal "Successfully deleted season #{season}.", flash[:notice]
+    refute Season.exists?(season.id)
+  end
+
+  test 'admin users can create seasons' do
+    admin_user = create(:user, admin: true)
+    admin_account = create(:oauth_account, user: admin_user)
+
+    assert_difference 'Season.count' do
+      sign_in_as(admin_account)
+      post '/admin/season', params: { create_season: {
+        max_rank: 5500, started_on: '2017-04-10', number: 3
+      } }
+    end
+
+    assert_redirected_to admin_path
+    assert_equal 'Successfully created season 3.', flash[:notice]
+    season = Season.find_by_number(3)
+    refute_nil season
   end
 
   test 'admin can merge two users' do
