654a38daf6df3610b0e849abb3cc00834f7d65c0
Sarah Vessels <cheshire137@gmail.com>
2018-03-03 18:45:27 -0600

    Test user setting their default account

--- test/controllers/oauth_accounts_controller_test.rb
+++ test/controllers/oauth_accounts_controller_test.rb
@@ -3,6 +3,41 @@ require 'test_helper'
 class OauthAccountsControllerTest < ActionDispatch::IntegrationTest
   fixtures :seasons
 
+  test 'anonymous user cannot set default account' do
+    put '/accounts/set-default'
+
+    assert_response :redirect
+    assert_redirected_to 'http://www.example.com/'
+  end
+
+  test 'authenticated user with multiple accounts can change their default account' do
+    user = create(:user)
+    oauth_account1 = create(:oauth_account, user: user)
+    oauth_account2 = create(:oauth_account, user: user)
+
+    sign_in_as(oauth_account1)
+    put '/accounts/set-default', params: { battletag: oauth_account2.to_param }
+
+    assert_equal oauth_account2, user.reload.default_oauth_account
+    assert_redirected_to accounts_path
+    assert_nil flash[:error]
+    assert_equal "Your default account is now #{oauth_account2}.", flash[:notice]
+  end
+
+  test 'cannot set your default account to an account not your own' do
+    user = create(:user)
+    oauth_account1 = create(:oauth_account, user: user)
+    oauth_account2 = create(:oauth_account)
+    user.default_oauth_account = oauth_account1
+    user.save!
+
+    sign_in_as(oauth_account1)
+    put '/accounts/set-default', params: { battletag: oauth_account2.to_param }
+
+    assert_response :not_found
+    assert_equal oauth_account1, user.reload.default_oauth_account
+  end
+
   test 'index loads for authenticated user' do
     user = create(:user)
     oauth_account1 = create(:oauth_account, user: user)
@@ -25,7 +60,7 @@ class OauthAccountsControllerTest < ActionDispatch::IntegrationTest
       delete "/accounts/#{oauth_account1.to_param}"
     end
 
-    assert_redirected_to '/accounts'
+    assert_redirected_to accounts_path
     assert_equal oauth_account2, user.reload.default_oauth_account
   end
 end
--- test/factories/oauth_accounts.rb
+++ test/factories/oauth_accounts.rb
@@ -3,6 +3,12 @@ FactoryBot.define do
     user
     provider 'bnet'
     uid { "12345#{OauthAccount.count}" }
-    battletag { user ? user.battletag : "SomeAccount#123#{OauthAccount.count}" }
+    battletag do
+      if user
+        "#{user.battletag}#{user.oauth_accounts.count}"
+      else
+        "SomeAccount#123#{OauthAccount.count}"
+      end
+    end
   end
 end
