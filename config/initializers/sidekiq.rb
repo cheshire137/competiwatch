2a3acd5f4b188ee512bab128d04bb024a03aad64
Sarah Vessels <cheshire137@gmail.com>
2018-03-10 16:16:38 -0600

    Adjust sidekiq config again
    http://manuelvanrijn.nl/sidekiq-heroku-redis-calc/

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,21 +4,6 @@ if Rails.env.production?
   end
 
   Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 4 }
-
-    Rails.application.config.after_initialize do
-      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      ActiveRecord::Base.connection_pool.disconnect!
-
-      ActiveSupport.on_load(:active_record) do
-        config = Rails.application.config.database_configuration[Rails.env]
-        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
-        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
-        config['pool'] = 16
-        ActiveRecord::Base.establish_connection(config)
-
-        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
-      end
-    end
+    config.redis = { url: ENV['REDIS_URL'], size: 9 }
   end
 end
