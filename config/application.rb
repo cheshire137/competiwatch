8b7d081883227a931b8e7aed96726d0dfca1aa14
Sarah Vessels <cheshire137@gmail.com>
2018-03-10 15:28:40 -0600

    Use sidekiq for active job backend

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,6 @@ module Competiwatch
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_job.queue_adapter = :sidekiq
   end
 end
