00d293cc2397678c4dcfba8203f3ac506ed1c9b1
Sarah Vessels <cheshire137@gmail.com>
2018-03-10 12:57:50 -0600

    Add #any_matches_for_user?

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,9 @@
 module AdminHelper
+  def any_matches_for_user?(matches_by_oauth_account_id, accounts_for_user)
+    account_ids_for_user = accounts_for_user.map(&:id)
+    matches_for_user = matches_by_oauth_account_id.select do |oauth_account_id, _matches|
+      account_ids_for_user.include?(oauth_account_id)
+    end
+    matches_for_user.any? { |_oauth_account_id, matches| matches.any? }
+  end
 end
