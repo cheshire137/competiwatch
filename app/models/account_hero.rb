532d687b05cefb847e8453a0af61e992fec7f6f6
Sarah Vessels <cheshire137@gmail.com>
2018-03-10 17:55:01 -0600

    Disallow more than 5 heroes per account

--- app/models/account_hero.rb
+++ app/models/account_hero.rb
@@ -1,7 +1,23 @@
 class AccountHero < ApplicationRecord
+  MAX_HEROES_PER_ACCOUNT = 5
+
   belongs_to :account
   belongs_to :hero
 
   validates :seconds_played, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
     allow_nil: true
+  validate :within_limit_per_account
+
+  private
+
+  def within_limit_per_account
+    return unless account
+
+    existing_account_heroes = account.account_heroes
+    existing_account_heroes = existing_account_heroes.where('id <> ?', id) if persisted?
+
+    if existing_account_heroes.count >= MAX_HEROES_PER_ACCOUNT
+      errors.add(:account, 'already has as many top heroes as is allowed per account')
+    end
+  end
 end
--- test/models/account_hero_test.rb
+++ test/models/account_hero_test.rb
@@ -1,10 +1,27 @@
 require 'test_helper'
 
 class AccountHeroTest < ActiveSupport::TestCase
+  fixtures :heroes
+
   test 'requires seconds_played >= 0' do
     account_hero = AccountHero.new(seconds_played: -1)
 
     refute_predicate account_hero, :valid?
     assert_includes account_hero.errors.messages[:seconds_played], 'must be greater than or equal to 0'
+  end
+
+  test 'disallows more than 5 heroes per account' do
+    account = create(:account)
+    create(:account_hero, account: account, hero: heroes(:symmetra))
+    create(:account_hero, account: account, hero: heroes(:mercy))
+    create(:account_hero, account: account, hero: heroes(:zenyatta))
+    create(:account_hero, account: account, hero: heroes(:mccree))
+    create(:account_hero, account: account, hero: heroes(:reinhardt))
+
+    account_hero = AccountHero.new(account: account, hero: heroes(:ana))
+
+    refute_predicate account_hero, :valid?
+    assert_includes account_hero.errors.messages[:account],
+      'already has as many top heroes as is allowed per account'
   end
 end
