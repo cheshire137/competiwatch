cafb2d90fe8519eaf4c74d55bb0d8704960766d7
Sarah Vessels <cheshire137@gmail.com>
2018-03-10 14:25:06 -0600

    Add helpers for showing avatar

--- app/helpers/oauth_accounts_helper.rb
+++ app/helpers/oauth_accounts_helper.rb
@@ -1,4 +1,25 @@
 module OAuthAccountsHelper
+  def avatar_link_for(oauth_account)
+    if oauth_account.avatar_url
+      link_to(avatar_image_for(oauth_account), profile_path(oauth_account), class: 'd-inline-block')
+    else
+      content_tag(:span, '', class: 'js-remote-load',
+                  data: { url: avatar_path(oauth_account, include_link: 1) })
+    end
+  end
+
+  def avatar_for(oauth_account)
+    if oauth_account.avatar_url
+      avatar_image_for(oauth_account)
+    else
+      content_tag(:span, '', class: 'js-remote-load', data: { url: avatar_path(oauth_account) })
+    end
+  end
+
+  def avatar_image_for(oauth_account)
+    image_tag(oauth_account.avatar_url, class: 'avatar mr-1', width: 20)
+  end
+
   def platform_options
     OAuthAccount::VALID_PLATFORMS.map { |key, label| [label, key] }
   end
