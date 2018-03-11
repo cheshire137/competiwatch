88db3c5f2b8607b4a04dd85b3613b6e36d9f62c2
Sarah Vessels <cheshire137@gmail.com>
2018-03-11 13:45:16 -0500

    Move mobile menu toggle into header

--- app/javascript/packs/dropdowns.js
+++ app/javascript/packs/dropdowns.js
@@ -1,23 +1,37 @@
 import {on} from 'delegated-events'
 
+function menuContainerFor(clickedEl) {
+  const menuToggle = clickedEl.closest('.js-menu-target')
+  if (!menuToggle) {
+    return
+  }
+
+  if (menuToggle.hasAttribute('data-target')) {
+    const selector = menuToggle.getAttribute('data-target')
+    return document.querySelector(selector)
+  }
+
+  return menuToggle.closest('.js-menu-container')
+}
+
 on('click', '.js-menu-target', function(event) {
   const button = event.currentTarget
-  const container = button.closest('.js-menu-container')
+  const container = menuContainerFor(button)
   container.classList.toggle('active')
 })
 
 document.addEventListener('click', function(event) {
-  const activeMenus = Array.from(document.querySelectorAll('.js-menu-container.active'))
-  if (activeMenus.length < 1) {
+  const activeMenuContainers = Array.from(document.querySelectorAll('.js-menu-container.active'))
+  if (activeMenuContainers.length < 1) {
     return
   }
 
   const clickedEl = event.target
-  const menuContainer = clickedEl.closest('.js-menu-container')
+  const menuContainer = menuContainerFor(clickedEl)
 
-  for (const activeMenu of activeMenus) {
-    if (activeMenu.closest('.js-menu-container') !== menuContainer) {
-      activeMenu.classList.remove('active')
+  for (const activeMenuContainer of activeMenuContainers) {
+    if (activeMenuContainer !== menuContainer) {
+      activeMenuContainer.classList.remove('active')
     }
   }
 })
