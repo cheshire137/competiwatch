a13a9802e9ff1f8137b41d45b30ea1e175ece2c8
Sarah Vessels <cheshire137@gmail.com>
2018-03-10 18:39:02 -0600

    Whoops fix latest account migration

--- db/migrate/20180310233105_rename_oauth_accounts.rb
+++ db/migrate/20180310233105_rename_oauth_accounts.rb
@@ -1,15 +1,15 @@
-class RenameAccounts < ActiveRecord::Migration[5.1]
+class RenameOAuthAccounts < ActiveRecord::Migration[5.1]
   def up
-    rename_table :accounts, :accounts
-    rename_column :matches, :account_id, :account_id
-    rename_column :season_shares, :account_id, :account_id
-    rename_column :users, :default_account_id, :default_account_id
+    rename_table :oauth_accounts, :accounts
+    rename_column :matches, :oauth_account_id, :account_id
+    rename_column :season_shares, :oauth_account_id, :account_id
+    rename_column :users, :default_oauth_account_id, :default_account_id
   end
 
   def down
-    rename_column :users, :default_account_id, :default_account_id
-    rename_column :season_shares, :account_id, :account_id
-    rename_column :matches, :account_id, :account_id
-    rename_table :accounts, :accounts
+    rename_column :users, :default_account_id, :default_oauth_account_id
+    rename_column :season_shares, :account_id, :oauth_account_id
+    rename_column :matches, :account_id, :oauth_account_id
+    rename_table :accounts, :oauth_accounts
   end
 end
