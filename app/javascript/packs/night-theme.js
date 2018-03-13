dededc53ce3de9fbba2d66964eaf59c7b4f72bb8
Sarah Vessels <cheshire137@gmail.com>
2018-03-12 23:30:52 -0500

    Start on a night theme

---
+++ app/javascript/packs/night-theme.js
@@ -0,0 +1,9 @@
+function setPageTheme() {
+  const date = new Date()
+  const hours = date.getHours()
+  const isNight = hours >= 21 || hours < 5
+  const themeClass = isNight ? 'theme-dark' : 'theme-light'
+  document.body.classList.add(themeClass)
+}
+
+setPageTheme()
