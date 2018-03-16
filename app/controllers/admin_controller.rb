841b0afc1e0be6198737921637759f95037e689b
Sarah Vessels <cheshire137@gmail.com>
2018-03-15 21:16:24 -0500

    Show top rank on admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,7 @@ class AdminController < ApplicationController
     all_users = User.order_by_battletag
     @latest_account = Account.order(id: :desc).where('user_id <> ?', current_user).first
     @top_accounts = Match.top_accounts
+    @top_rank = Account.top_rank
     @friend_count = Friend.count
     @match_count = Match.count
     @latest_matches = Match.joins(:account).where('accounts.user_id <> ?', current_user.id).
