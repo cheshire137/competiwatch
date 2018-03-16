fc68c7558aca81a9cbf73134a744425095892472
Sarah Vessels <cheshire137@gmail.com>
2018-03-15 21:46:15 -0500

    Drop account_heroes table

---
+++ db/migrate/20180316024425_drop_account_heroes.rb
@@ -0,0 +1,17 @@
+class DropAccountHeroes < ActiveRecord::Migration[5.1]
+  def up
+    drop_table :account_heroes
+  end
+
+  def down
+    create_table :account_heroes do |t|
+      t.integer :account_id, null: false
+      t.integer :hero_id, null: false
+      t.integer :seconds_played
+      t.timestamps
+    end
+    add_index :account_heroes, [:account_id, :hero_id], unique: true
+    add_index :account_heroes, :hero_id
+    add_index :account_heroes, [:account_id, :seconds_played]
+  end
+end
