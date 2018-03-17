6d7476b877449763caeaa84720726c5d64fba059
Sarah Vessels <cheshire137@gmail.com>
2018-03-16 23:10:15 -0500

    Remove unused #time_zone_options helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,4 @@
 module UsersHelper
-  def time_zone_options
-    ActiveSupport::TimeZone.all.map do |zone|
-      offset = ActiveSupport::TimeZone.seconds_to_utc_offset(zone.utc_offset)
-      label = "#{zone.name} (#{offset})"
-      [label, zone.name]
-    end
-  end
-
   def settings_box(icon_class:, title:, url:, description:)
     render partial: 'users/settings_box',
            locals: { icon_class: icon_class, title: title,
