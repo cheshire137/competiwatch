2328c316b260a82b58fd33ecc6046a9d70957bc7
Sarah Vessels <cheshire137@gmail.com>
2018-03-16 22:56:18 -0500

    Remove friend ID from match group members on friend delete

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -8,7 +8,18 @@ class Friend < ApplicationRecord
 
   scope :order_by_name, ->{ order('LOWER(name) ASC') }
 
+  after_destroy :remove_from_matches
+
   def matches
     Match.joins(:account).where(accounts: { user_id: user_id }).with_group_member(self)
+  end
+
+  private
+
+  def remove_from_matches
+    matches.each do |match|
+      match.group_member_ids -= [id]
+      match.save
+    end
   end
 end
