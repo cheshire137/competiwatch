6a914ef8e0826404b426fc02347bdab1478a87f2
Sarah Vessels <cheshire137@gmail.com>
2018-03-18 16:18:25 -0500

    Indicate placement/placement log on admin page

--- test/controllers/admin/home_controller_test.rb
+++ test/controllers/admin/home_controller_test.rb
@@ -27,11 +27,18 @@ class Admin::HomeControllerTest < ActionDispatch::IntegrationTest
     friend = create(:friend, user: user)
     match2 = create(:match, result: :win, season: 2, account: account,
                     group_member_ids: [friend.id], heroes: [heroes(:ana)])
+    placement_match = create(:match, placement: true, season: 1)
+    placement_log_match = create(:match, placement: false, season: 1, map: nil,
+                                 prior_match: nil, rank: 3000)
 
     sign_in_as(admin_account)
     get '/admin'
 
     assert_response :ok
     assert_select 'button', text: /#{admin_account.name}/
+    assert_select '.test-latest-matches span', text: 'Placement log'
+    assert_select '.test-latest-matches span', text: 'Placement'
+    assert_select '.test-latest-matches span', text: 'Win'
+    assert_select '.test-latest-matches span', text: '1 hero'
   end
 end
