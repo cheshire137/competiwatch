82c5cdff60399a5c86c5709c635a3e86eb3bbbb1
Sarah Vessels <cheshire137@gmail.com>
2018-03-18 16:48:49 -0500

    Show top heroes on community page

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -3,5 +3,6 @@ class CommunityController < ApplicationController
     @top_rank = Account.top_rank
     @bottom_rank = Account.bottom_rank
     @average_rank = Account.average_rank
+    @top_heroes = Hero.most_played
   end
 end
