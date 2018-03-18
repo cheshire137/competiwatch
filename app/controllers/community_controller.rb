c077eed087ca97f9ef8ccd7e22043cc4baae278e
Sarah Vessels <cheshire137@gmail.com>
2018-03-18 16:46:06 -0500

    Show rank stats on community page

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,4 +1,7 @@
 class CommunityController < ApplicationController
   def index
+    @top_rank = Account.top_rank
+    @bottom_rank = Account.bottom_rank
+    @average_rank = Account.average_rank
   end
 end
