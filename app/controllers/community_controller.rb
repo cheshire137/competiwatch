61d08a6a4b4d78fd0659a5902d41ce363ed0ff76
Sarah Vessels <cheshire137@gmail.com>
2018-03-18 19:16:09 -0500

    Show weekend win percentage on community page

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -3,8 +3,10 @@ class CommunityController < ApplicationController
     @top_rank = Account.top_rank
     @bottom_rank = Account.bottom_rank
     @average_rank = Account.average_rank
+
     @season_number = Season.current_or_last_number
     @top_heroes = Hero.most_played(season: @season_number)
     @thrower_leaver_percent = Match.thrower_leaver_percent(season: @season_number)
+    @weekend_win_percent = Match.weekend_win_percent(season: @season_number)
   end
 end
