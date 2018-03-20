4edfc90e50b9b15d25c92426b9697b61eae37497
Sarah Vessels <cheshire137@gmail.com>
2018-03-20 09:59:04 -0500

    Don't show lowest rank

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -3,7 +3,6 @@ class CommunityController < ApplicationController
 
   def index
     @top_rank = Account.top_rank
-    @bottom_rank = Account.bottom_rank
     @average_rank = Account.average_rank
 
     @season_number = Season.current_or_last_number
