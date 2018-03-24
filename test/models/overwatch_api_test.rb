65e27833848d1fe20df62ffc93df86004988caa3
Sarah Vessels <cheshire137@gmail.com>
2018-03-24 11:41:43 -0500

    Update failing tests

--- test/models/overwatch_api_test.rb
+++ test/models/overwatch_api_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class OverwatchAPITest < ActiveSupport::TestCase
   test 'battletag is encoded for URL' do
-    api = OverwatchAPI.new(battletag: 'Amélie#1234', region: 'us', platform: 'pc')
-    assert_equal '/profile/pc/us/Am%C3%A9lie-1234', api.profile_url
+    api = OverwatchAPI.new(battletag: 'Amélie#1234', platform: 'pc')
+    assert_equal '/api/v3/u/Am%C3%A9lie-1234/stats?platform=pc', api.profile_url
   end
 end
