b1fb12820a93213f9d93e92364fea78a1998cb76
Sarah Vessels <cheshire137@gmail.com>
2018-03-25 16:00:10 -0500

    Customize 403 error

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,7 @@ Rails.application.routes.draw do
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
   match '/401', to: 'errors#unauthorized', via: :all
+  match '/403', to: 'errors#forbidden', via: :all
   match '/422', to: 'errors#unprocessable_entity', via: :all
   match '*path', to: 'errors#not_found', via: :all
 
