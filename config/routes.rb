35b44d519ae89913614942fa74779a4fb0bf416d
Sarah Vessels <cheshire137@gmail.com>
2018-03-25 15:59:52 -0500

    Customize 401 error

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,7 @@ Rails.application.routes.draw do
 
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#internal_server_error', via: :all
+  match '/401', to: 'errors#unauthorized', via: :all
   match '/422', to: 'errors#unprocessable_entity', via: :all
   match '*path', to: 'errors#not_found', via: :all
 
