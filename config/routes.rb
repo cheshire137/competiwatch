a06804b4f8f7759243eb2351ad2b5c09b6d17570
Sarah Vessels <cheshire137@gmail.com>
2018-03-25 15:59:16 -0500

    Add custom 500 page

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Rails.application.routes.draw do
   get '/help' => 'pages#help', as: :help
 
   match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
   match '/422', to: 'errors#unprocessable_entity', via: :all
   match '*path', to: 'errors#not_found', via: :all
 
