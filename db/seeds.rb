d627d4419e05461f7822d6bb596dcb46275d1783
Sarah Vessels <cheshire137@gmail.com>
2018-07-27 21:30:56 -0500

    Update seeds to include season 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -196,7 +196,8 @@ seasons = {
   7 => { max_rank: 5000, started_on: '2017-11-01', ended_on: '2017-12-29' },
   8 => { max_rank: 5000, started_on: '2018-01-01', ended_on: '2018-02-25' },
   9 => { max_rank: 5000, started_on: '2018-02-28', ended_on: '2018-04-27' },
-  10 => { max_rank: 5000, started_on: '2018-04-30' }
+  10 => { max_rank: 5000, started_on: '2018-04-30', ended_on: '2018-06-27' },
+  11 => { max_rank: 5000, started_on: '2018-06-30' }
 }
 
 seasons.each do |number, data|
